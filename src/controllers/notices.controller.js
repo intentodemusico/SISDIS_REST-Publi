c19e2a1201c9806025cc38ebb4cb093d41d0d79c
Oswaldo Andrés <48687178+Oswalman@users.noreply.github.com>
2020-03-30 15:20:32 -0500

    Crear

--- src/controllers/notices.controller.js
+++ src/controllers/notices.controller.js
@@ -1,29 +1,39 @@
 const notesCtrl = {};
-const Notice= require('../models/Notices');
-
-
+const Notice = require('../models/Notices');
 
 notesCtrl.getNotes = async (req, res) => {
-   
-    const lostPet=await Notice.find();
-    res.json(lostPet);
+	const notice = await Notice.find();
+	res.json(notice);
 };
 
 notesCtrl.createNote = async (req, res) => {
-    res.send("crepost");
+	
+	const noticeData = {
+		titular: req.body.titular,
+		autor: req.body.autor,
+		contenido: req.body.contenido
+	};
+	Notice.create(noticeData)
+		.then((notice) => {
+			console.log('Registró',notice);
+		})
+		.catch((err) => {
+			console.log('Error al crear');
+			res.send('error: ' + err);
+		});
 };
 
 notesCtrl.getNote = async (req, res) => {
-    res.send("getonepost");
-}
+	res.send('getonepost');
+};
 
 notesCtrl.deleteNote = async (req, res) => {
-    res.send("depost");
-}
+	res.send('depost');
+};
 
 notesCtrl.updateNote = async (req, res) => {
-   
-    res.json('Note Updated');
-}
+    const { _id, titular, autor, contenido } = req.body;
+	res.json('Note Updated');
+};
 
 module.exports = notesCtrl;
