e37b66c9a185b4714a0d8b4013272556b3a4d969
Oswaldo Andrés <48687178+Oswalman@users.noreply.github.com>
2020-03-30 15:02:18 -0500

    Correción password

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const URI = process.env.MONGOOSE_URI
     ? process.env.MONGOOSE_URI
-    : 'mongodb://Admin:<informatica>@rmi-shard-00-00-p8iu2.mongodb.net:27017,rmi-shard-00-01-p8iu2.mongodb.net:27017,rmi-shard-00-02-p8iu2.mongodb.net:27017/test?ssl=true&replicaSet=RMI-shard-0&authSource=admin&retryWrites=true&w=majority';
+    : 'mongodb://Admin:Informatica@rmi-shard-00-00-p8iu2.mongodb.net:27017,rmi-shard-00-01-p8iu2.mongodb.net:27017,rmi-shard-00-02-p8iu2.mongodb.net:27017/test?ssl=true&replicaSet=RMI-shard-0&authSource=admin&retryWrites=true&w=majority';
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
