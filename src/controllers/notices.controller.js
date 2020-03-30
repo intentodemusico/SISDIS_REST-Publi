2c9e06c3bf5fb20305b92de7dabc1ac0a0c733aa
? <alberto.garcia.2016@upb.edu.co>
2020-03-30 18:55:40 -0500

    UltimateFinder

--- src/controllers/notices.controller.js
+++ src/controllers/notices.controller.js
@@ -1,77 +1,58 @@
 const notesCtrl = {};
 const Notice = require('../models/Notices');
 
-notesCtrl.test = async (req, res) => {
-    res.json("{res:1}");
-};
-
-notesCtrl.t=async (req, res) => {
-    res.send('hello world');
-};
-
 notesCtrl.findNotes = async (req, res) => {
-    const noticeData = {
-        search: req.body.search}
-        
-    var selector = {"Search": {$regex: /.*/, $options:"i"}}
-    Notice.findOne().then(() => {
-        console.log('Registró');
-    })
-    .catch((err) => {
-        console.log('Error al crear');
-        res.send('error: ' + err);
-        res.json(notice);
-    });
-	
+    if (req.query == {}) {
+        res.send('error');
+    }   else {
+        var query ={$or:[ { titular: "" }, {autor:""}, { contenido: "" } ] };
+        query.$or[0].titular= req.query.titular && req.query.titular.length>0 ? new RegExp(req.query.titular) : ""
+        query.$or[1].autor=req.query.autor && req.query.autor.length>0 ? new RegExp(req.query.autor) : ""
+        query.$or[2].contenido= req.query.contenido && req.query.contenido.length>0 ? new RegExp(req.query.contenido) : ""
+        console.log(query)
+        Notice.find(query)
+            .then((notice) => {
+                res.json(notice);
+            })
+            .catch((err) => {
+                res.send('error: ' + err);
+            });
+    }
 };
-
 notesCtrl.getNotes = async (req, res) => {
-	const notice = await Notice.find();
-	res.json(notice);
+    const notice = await Notice.find();
+    res.json(notice);
 };
 
 notesCtrl.createNote = async (req, res) => {
-	
-	const noticeData = {
-		titular: req.body.titular,
-		autor: req.body.autor,
-		contenido: req.body.contenido
-	};
-	Notice.create(noticeData)
-		.then((notice) => {
-			console.log('Registró',notice);
-		})
-		.catch((err) => {
-			console.log('Error al crear');
-			res.send('error: ' + err);
-		});
-};
 
-notesCtrl.getNote = async (req, res) => {
-	const titular=req.body.titular;
-    const autor=req.body.autor;
-    const contenido=req.body.contenido;
-    if (titular!= null){
-        const find=Notice.findOne(titular);
-        return find;
-    }
-    if (autor!= null){
-        const find=Notice.findOne(autor);
-        return find;
-    }
-    if (contenido!= null){
-        const find=Notice.findOne(contenido);
-        return find;
-    }
+    const noticeData = {
+        titular: req.body.titular,
+        autor: req.body.autor,
+        contenido: req.body.contenido
+    };
+    Notice.create(noticeData)
+        .then((notice) => {
+            console.log('Registró', notice);
+        })
+        .catch((err) => {
+            console.log('Error al crear');
+            res.send('error: ' + err);
+        });
 };
 
 notesCtrl.deleteNote = async (req, res) => {
-	res.send('depost');
+    res.send('depost');
 };
 
 notesCtrl.updateNote = async (req, res) => {
-    const { _id, titular, autor, contenido } = req.body;
-	res.json('Note Updated');
+    const {
+        _id,
+        titular,
+        autor,
+        contenido
+    } = req.body;
+    res.json('Note Updated');
 };
 
 module.exports = notesCtrl;
--- src/routes/notices.js
+++ src/routes/notices.js
@@ -1,20 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getNotes, createNote, getNote,findNotes, t,deleteNote, test, updateNote } = require('../controllers/notices.controller');
+const { getNotes, createNote,findNotes,deleteNote, updateNote } = require('../controllers/notices.controller');
 
 router.route('/')
     .get(getNotes)    
     .post(createNote);
-router.route("/test")
-    .get(test);
-
-router.route('/t')
-    .get(t);
-
 
 router.route('/:id')
-    .get(getNote)
     .delete(deleteNote)
     .put(updateNote);
 
